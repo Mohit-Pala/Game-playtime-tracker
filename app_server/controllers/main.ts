1d0836036f7005f10a72bdc6317f39540e5a7a34
Mohit-Pala <palav@appstate.edu>
2024-04-08 10:39:21 -0400

    deleted everything

--- app_server/controllers/main.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export default class MainCtrl {
-    index = (req: Request, res: Response, next: NextFunction) => {
-        res.render('index', { title: 'Express' });
-    }
-}
--- app_server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import MainCtrl from '../controllers/main';
-
-const router = express.Router();
-const mainCtrl = new MainCtrl();
-
-router.get('/', mainCtrl.index);
-
-export default router;
