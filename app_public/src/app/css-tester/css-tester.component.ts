37f0780dc0d5d9e9276c1ceed6156857ab6b482c
Mohit-Pala <palav@appstate.edu>
2024-04-19 11:15:23 -0400

    jank way to store imahes

--- app_public/src/app/css-tester/css-tester.component.ts
+++ app_public/src/app/css-tester/css-tester.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './css-tester.component.css'
 })
 export class CssTesterComponent {
+  file?: any
+  base64Stuff?: String
 
+  getImage(event: any) {
+    this.file = event.target.files[0]
+  }
+
+  updateImage() {
+    const reader = new FileReader()
+    reader.readAsDataURL(this.file)
+
+    reader.onloadend = () => {
+      const base64String = reader.result as string
+      console.log(base64String)
+      this.base64Stuff = base64String
+    }
+  }
 }
