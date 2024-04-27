219aa8d06dcb4b976c7725ea640ecb11a560b7b1
Mohit-Pala <palav@appstate.edu>
2024-04-27 14:15:50 -0400

    added sample images for debug

--- app.ts
+++ app.ts
@@ -15,15 +15,15 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(function(req: Request, res: Response, next: NextFunction) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, content-type, Accept, Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 })
 app.use(express.urlencoded({ extended: false }));
@@ -48,5 +48,7 @@ app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(express.json({ limit: '100mb' }));
 
 export default app;
